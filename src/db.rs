75883ae83e376c1556761087ffeb157540d842f6
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-09-24 18:32:34 +0100

    Add mock data

--- src/db.rs
+++ src/db.rs
@@ -4,3 +4,19 @@ use tokio::sync::Mutex;
 use crate::models::Customer;
 
 pub type Db = Arc<Mutex<Vec<Customer>>>;
+
+use std::fs::File;
+use serde_json::from_reader;
+
+pub fn init_db() -> Db {
+    let file = File::open("./data/customers.json");
+    match file {
+        Ok(json) => {
+            let customers = from_reader(json).unwrap();
+            Arc::new(Mutex::new(customers))
+        },
+        Err(_) => {
+            Arc::new(Mutex::new(Vec::new()))
+        }
+    }
+}
