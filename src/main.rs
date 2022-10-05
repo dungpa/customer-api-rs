3e68ca4740de019eecee806a21c60e25d2714d99
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-10-05 21:22:04 +0100

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,31 @@ mod models;
 mod db;
 mod routes;
 
+ use rocket::*;
+
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![routes::list_customers])
+}
+
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build()
-        .mount("/", routes![routes::list_customers])
+    let _rocket = rocket()
         .ignite().await?
         .launch().await?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn list_customers_returns_correct_results() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!("/customers")).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
 }
