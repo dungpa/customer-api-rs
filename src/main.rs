10bec1a8ca762bb8f2ac1e0bcf0f179dc450cefc
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-10-05 21:32:42 +0100

    Implement another get endpoint

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod routes;
  use rocket::*;
 
 fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![routes::list_customers])
+    rocket::build().mount("/", routes![routes::list_customers, routes::get_customer])
 }
 
 #[rocket::main]
@@ -36,5 +36,19 @@ mod test {
         let client = Client::tracked(rocket()).expect("valid rocket instance");
         let response = client.get(uri!("/customers")).dispatch();
         assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn get_customer_returns_correct_results_on_known_id() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!("/customer/4622d0f0-aad4-4c10-a6df-415232141866")).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn get_customer_returns_correct_results_on_unknown_id() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!("/customer/123")).dispatch();
+        assert_eq!(response.status(), Status::NotFound);
     }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,6 @@
 use crate::models::*;
 use crate::db::*;
-//use rocket::response::content::Json;
+use rocket::response::status;
 use rocket::serde::json::*;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -8,9 +8,26 @@ pub struct CustomerListResponse {
     pub customers: Vec<Customer>,
 }
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct CustomerResponse {
+    pub customer: Customer,
+}
+
 #[get("/customers")]
 pub async fn list_customers() -> Json<CustomerListResponse> {
     let db = init_db();
     let customers = db.lock().await;
     Json(CustomerListResponse { customers: customers.to_vec() })
+}
+
+#[get("/customer/<guid>")]
+pub async fn get_customer(guid: String) -> Result<Json<CustomerResponse>, status::NotFound<String>> {
+    let db = init_db();
+    let customers = db.lock().await;
+    for customer in customers.to_vec() {
+        if customer.guid == guid {
+            return Ok (Json(CustomerResponse { customer }))
+        }
+    }
+    Err(status::NotFound(format!("Error getting customer {}.", guid)))
 }
